d338c4287b6defe1f1736cfc533fb2a05659ed2f
jpsm83 <jpsm83@hotmail.com>
2021-11-23 18:02:55 +0100

    front css done

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,26 +1,41 @@
 import { useState } from "react";
 import Image from "next/image";
 import { StarIcon } from "@heroicons/react/outline";
+import Currency from "react-currency-formatter";
 
 function Product({ id, title, price, description, category, image }) {
   const [rating, setRating] = useState(Math.floor(Math.random() * 5));
+  const [prime, setPrime] = useState(Math.random() < 0.5);
 
   const stars = [];
-  for (let i=0; i<rating; i++) {
-      stars.push(<StarIcon className="h-5" />)
+  for (let i = 0; i < rating; i++) {
+    stars.push(<StarIcon className="h-5 text-yellow-500" />);
   }
 
   return (
-    <div>
-      <p>{category}</p>
+    <div className="relative flex flex-col m-5 bg-white z-30 p-10">
+      <p className="absolute top-2 right-2 text-xs italic text-gray-400">{category}</p>
 
       <Image src={image} height={200} width={200} objectFit="contain" />
 
-      <h4>{title}</h4>
+      <h4 className="my-3">{title}</h4>
 
-      <div className="flex">
-        {stars}
+      <div className="flex">{stars}</div>
+
+      <p className="text-xs my-2 line-clamp-2">{description}</p>
+
+      <div className="mb-5">
+        <Currency quantity={price} currency="EUR" />
       </div>
+
+      {prime && (
+        <div className="flex items-center space-x-2 mt-5">
+          <img className="w-12" src="/images/prime.png" alt="Prime" />
+          <p className="text-xs text-gray-500">FREE Next-day Delivery</p>
+        </div>
+      )}
+
+      <button className="mt-auto button">Add to Basket</button>
     </div>
   );
 }
