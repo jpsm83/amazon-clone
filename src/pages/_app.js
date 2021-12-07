244cc6030c358f8c97d16a0385e735a06e3d6944
jpsm83 <jpsm83@hotmail.com>
2021-12-07 11:09:34 +0100

    google auth

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,6 +6,7 @@ import { Provider as AuthProvider } from "next-auth/client";
 
 const MyApp = ({ Component, pageProps }) => {
   return (
+    // AuthProvider wrap the entire app and give it access to next authentication for all of it
     // hight order component - parent component has children components in it
     <AuthProvider session={pageProps.session}>
       <Provider store={store}>
